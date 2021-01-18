75504c59d7027c43ab1d4953f98c1b212bf3801e
Sayan Ghosh <sayan.jucse@gmail.com>
2021-01-18 23:18:50 +1100

    Updating Role name as per MSFT API changes (see https://docs.microsoft.com/en-us/answers/questions/233909/get-azureaddirectoryrole-and-34global-administrato.html)

--- TestCode/AADDSinputValidation.ps1
+++ TestCode/AADDSinputValidation.ps1
@@ -15,14 +15,44 @@ Additionally, this script assigns the subscription Contributor role to the WVDSe
 $SubscriptionId = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
 $fileURI = Get-AutomationVariable -Name 'fileURI'
-$domainName = Get-AutomationVariable -Name 'domainName'
 $resourceTags = Get-AutomationVariable -Name 'tags'
+
+
+Write-Output "Read Variables $SubscriptionId $ResourceGroupName $fileURI $resourceTags"
+
+# Handle the tags from Json for various PowerShell commandlets to be able to handle
+#Hashtable tags for Azure cmdlets which would not work with the JObject that the tags are parsed with by default
+[hashtable]$hashTags = $null
+$hashTags = @{}
+[int]$loopindex = 0
+#Enumerate through Json JObject and create String and Hashtable versions
+foreach ($tags in $resourceTags.GetEnumerator()) {
+    #Write-Output $loopindex
+	#Write-Output @($resourceTags)[$loopindex].ToString()
+	#[string]$thistag = @($resourceTags)[$loopindex].ToString() -replace ": ", "="
+	#$tagsValue = "{0}{1}{2}" -f $tagsValue, $thistag,"; "
+    #Write-Output $thistag
+	$hashTags.add((@($resourceTags)[$loopindex].ToString().Split(':'))[0].Trim(),(@($resourceTags)[$loopindex].ToString().Split(':'))[1].Trim())
+    #increment the loop for the next JObject item
+    $loopindex++    
+}
+
+#$tagsValue = "{0}{1}" -f $tagsValue, "}"
+#$tagsValue = $tagsValue -replace "; }", "}"
+#Write-Output $tagsValue
+#Write-Output $hashTags
+#"Microsoft.PowerShell.Core", "Microsoft.PowerShell.Host" | ForEach-Object {
+#	Write-Output $loopindex
+#	$_.Split(".")
+#	$loopindex++
+#}
 
 # Download files required for this script from github ARMRunbookScripts/static folder
 $FileNames = "msft-wvd-saas-api.zip,msft-wvd-saas-web.zip,AzureModules.zip"
 $SplitFilenames = $FileNames.split(",")
 foreach($Filename in $SplitFilenames){
-Invoke-WebRequest -Uri "$fileURI/ARMRunbookScripts/static/$Filename" -OutFile "C:\$Filename"
+	Write-Output "$fileURI/ARMRunbookScripts/static/$Filename"
+	Invoke-WebRequest -Uri "$fileURI/ARMRunbookScripts/static/$Filename" -OutFile "C:\$Filename"
 }
 
 #New-Item -Path "C:\msft-wvd-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
@@ -125,79 +155,14 @@ foreach($resourceProvider in $wvdResourceProviderName) {
 }
 #endregion
 
-$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-
-$username = "tempUser@" + $domainName
-
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AzCredentials.password)
-$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-$PasswordProfile.Password = $UnsecurePassword
-$PasswordProfile.ForceChangePasswordNextLogin = $False
-
-New-AzureADUser -DisplayName $username -PasswordProfile $PasswordProfile -UserPrincipalName $username -AccountEnabled $true -MailNickName "tempUser"
-
-$domainUser = Get-AzureADUser -Filter "UserPrincipalName eq '$($username)'" | Select-Object ObjectId
-# Fetch user to assign to role
-$roleMember = Get-AzureADUser -ObjectId $domainUser.ObjectId
-
-# Fetch User Account Administrator role instance
-$role = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq 'Global Administrator'}
-# If role instance does not exist, instantiate it based on the role template
-if ($null -eq $role) {
-    # Instantiate an instance of the role template
-    $roleTemplate = Get-AzureADDirectoryRoleTemplate | Where-Object {$_.displayName -eq 'Global Administrator'}
-    Enable-AzureADDirectoryRole -RoleTemplateId $roleTemplate.ObjectId
-    # Fetch User Account Administrator role instance again
-    $role = Get-AzureADDirectoryRole | Where-Object {$_.displayName -eq 'Global Administrator'}
-}
-# Add user to role
-Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId $roleMember.ObjectId
-# Fetch role membership for role to confirm
-Get-AzureADDirectoryRoleMember -ObjectId $role.ObjectId | Get-AzureADUser
-
-New-AzADServicePrincipal -ApplicationId "2565bd9d-da50-47d4-8b85-4c97f669dc36"
-
-# Create domain controller admin group
-New-AzureADGroup -DisplayName "AAD DC Administrators" `
-                 -Description "Delegated group to administer Azure AD Domain Services" `
-                 -SecurityEnabled $true -MailEnabled $false `
-                 -MailNickName "AADDCAdministrators"
-
-# Add user to "AAD DC Administrators" group
-
-# First, retrieve the object ID of the newly created 'AAD DC Administrators' group.
-$GroupObjectId = Get-AzureADGroup -Filter "DisplayName eq 'AAD DC Administrators'" | Select-Object ObjectId
-
-# Add the user to the 'AAD DC Administrators' group.
-Add-AzureADGroupMember -ObjectId $GroupObjectId.ObjectId -RefObjectId $domainUser.ObjectId
-
-# Grant managed identity contributor role on subscription level
-$identity = Get-AzUserAssignedIdentity -ResourceGroupName $ResourceGroupName -Name "WVDServicePrincipal"
-New-AzRoleAssignment -RoleDefinitionName "Contributor" -ObjectId $identity.PrincipalId -Scope "/subscriptions/$subscriptionId"
-Start-Sleep -Seconds 5
-
-# Handle the tags from Json for various PowerShell commandlets to be able to handle
-#Hashtable tags for Azure cmdlets which would not work with the JObject that the tags are parsed with by default
-[hashtable]$hashTags = $null
-$hashTags = @{}
-[int]$loopindex = 0
 # Set up an Azure Policy at the resource group level for the resources to inherit tags from the parent resource group
 
-$resourcegroup = Get-AzResourceGroup -Name $ResourceGroupName$definition = New-AzPolicyDefinition -Name "inherit-resourcegroup-tag-if-missing" -DisplayName "Inherit a tag from the resource group if missing" -description "Adds the specified tag with its value from the parent resource group when any resource missing this tag is created or updated. Existing resources can be remediated by triggering a remediation task. If the tag exists with a different value it will not be changed." -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/Tags/inherit-resourcegroup-tag-if-missing/azurepolicy.rules.json' -Parameter 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/Tags/inherit-resourcegroup-tag-if-missing/azurepolicy.parameters.json' -Mode Indexed
+$resourcegroup = Get-AzResourceGroup -Name $ResourceGroupName
 
-#Enumerate through Json JObject and create String and Hashtable versions / assign policy
-foreach ($tags in $resourceTags.GetEnumerator()) {
-	#Retrieve tag values from Json, replace " and split by :
-	$individualtagName = (@($resourceTags)[$loopindex].ToString().Split(':'))[0].Trim()  -replace "\""", ""
-	$individualtagValue = (@($resourceTags)[$loopindex].ToString().Split(':'))[1].Trim()  -replace "\""", ""
-	$hashTags.add($individualtagName, $individualtagValue)
-	
-	#Assign Azure Policy for this tag to be inherited from this resource group to all resources, this needs to be done manually from all tags
-	New-AzPolicyAssignment -Name "Inherit tags $individualtagName from $ResourceGroupName to all resources" -Scope $resourcegroup.ResourceId -tagName $individualtagName -PolicyDefinition $definition -AssignIdentity -Location australiaeast
+#Write-Output  $resourcegroup.ResourceId
 
-  	$loopindex++    
-}
-
+$definition = New-AzPolicyDefinition -Name "inherit-resourcegroup-tag-if-missing" -DisplayName "Inherit a tag from the resource group if missing" -description "Adds the specified tag with its value from the parent resource group when any resource missing this tag is created or updated. Existing resources can be remediated by triggering a remediation task. If the tag exists with a different value it will not be changed." -Policy 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/Tags/inherit-resourcegroup-tag-if-missing/azurepolicy.rules.json' -Parameter 'https://raw.githubusercontent.com/Azure/azure-policy/master/samples/Tags/inherit-resourcegroup-tag-if-missing/azurepolicy.parameters.json' -Mode Indexed
+New-AzPolicyAssignment -Name "Inherit the resource group tags for all WVD resources" -Scope $resourcegroup.ResourceId -tagName $hashTags -PolicyDefinition $definition -AssignIdentity -Location australiaeast
 
 # Tag the resource group with the user specified tags
 Set-AzResourceGroup -Name $ResourceGroupName -Tag $hashTags
